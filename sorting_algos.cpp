5fdc255f1c32d20aa2923e04f6f18ef3e6b5b36b
Henil Valand <henilvaland@gmail.com>
2025-12-07 18:45:57 +0530

    added quick sort implementation

--- sorting_algos.cpp
+++ sorting_algos.cpp
@@ -93,11 +93,37 @@ void merge_sort(int arr[], int low, int high){
     merge(arr, low, mid, high);
 }
 
+int partition(int arr[], int low, int high){
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while(i < j){
+        while(arr[i]<=pivot && i <= high - 1){
+            i++;
+        }
+        while(arr[j] > pivot && j >= low + 1){
+            j--;
+        }
+        if(i < j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void quick_sort(int arr[], int low, int high){
+    //divide and conquer algorithm
+    if(low < high){
+        int partitionIndex = partition(arr, low, high);
+        quick_sort(arr, low, partitionIndex - 1);
+        quick_sort(arr, partitionIndex + 1, high);
+    }
+}
+
 int main(){
     int arr[] = {5, 2, 9, 1, 5, 6};
     int n = 6;
     //insertion_sort(arr, n);
-    merge_sort(arr, 0, n-1);
+    quick_sort(arr, 0, n-1); //merge_sort(arr, 0, n-1);
     cout << "Sorted Array is --> ";
     for(int i = 0; i < n; i++){
         cout << arr[i] << " ";
