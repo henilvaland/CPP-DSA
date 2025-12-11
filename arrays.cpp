ebce2d74b745a1a4c90611ada99c791be760b28f
Henil Valand <henilvaland@gmail.com>
2025-12-11 22:20:51 +0530

    added largest Elem function

--- arrays.cpp
+++ arrays.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int linearSearch(vector<int>& nums, int target) {
@@ -10,10 +10,16 @@ int linearSearch(vector<int>& nums, int target) {
         return -1;
     }
 
+int largestElement(std::vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int largest = nums[nums.size() - 1];
+    return largest;
+}
+
 int main() {
-    vector<int> nums = {1, 2, 3, 4, 5};
+    vector<int> nums = {1, 2, 7, 4, 5};
     int target = 4;
-    int result = linearSearch(nums, target);
+    int result = largestElement(nums);
     cout << result << endl;
     return 0;
 }
