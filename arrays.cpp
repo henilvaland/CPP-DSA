4975ba70f7355261078513224c697e50324eefc7
Henil Valand <henilvaland@gmail.com>
2025-12-30 08:03:02 +0530

    union array

--- arrays.cpp
+++ arrays.cpp
@@ -47,12 +47,53 @@ int removeDuplicates(vector<int>& nums) {
     return i+1;
 }
 
+vector<int> unionArray(vector<int>& nums1, vector<int>& nums2) {
+    int n1 = nums1.size();
+    int n2 = nums2.size();
+    int i = 0;
+    int j = 0;
+    vector<int> unionArr;
+
+    while(i < n1 && j < n2){
+        if(nums1[i] <= nums2[j]){
+            if(unionArr.size() == 0 || unionArr.back() != nums1[i]){
+                unionArr.push_back(nums1[i]);
+            }
+            i++;
+        }
+        else {
+            if(unionArr.size() == 0 || unionArr.back() != nums2[j]){
+                unionArr.push_back(nums2[j]);
+            }
+            j++;
+        }
+    }
+
+    while(i < n1){
+        if(unionArr.size() == 0 || unionArr.back() != nums1[i]){
+            unionArr.push_back(nums1[i]);
+        }
+        i++;
+    }
+
+    while(j < n2){
+        if(unionArr.size() == 0 || unionArr.back() != nums2[j]){
+            unionArr.push_back(nums2[j]);
+        }
+        j++;
+    }
+
+    return unionArr;
+}
+
 int main() {
     vector<int> nums = {1, 0, 2, 7, 0, 4, 5};
+    vector<int> nums2 = {1, 2, 7};
     int target = 4;
-    int missingNum = missingNumber(nums);
+    var unionArr = unionArray(nums, nums2); 
+    //int missingNum = missingNumber(nums);
     //for(int i=  0; i < nums.size(); i++){
-        cout << missingNum << " ";
+        //cout << missingNum << " ";
     //}
     cout << endl;
     return 0;
